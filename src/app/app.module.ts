c40531520e8160747eb93d97f3fa2a1216e4450c
Darian Everett <deverett@1800contacts.com>
2020-01-30 16:22:12 -0700

    work in progress on header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderNavigationComponent } from './header-navigation/header-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavigationComponent
   ],
   imports: [
     BrowserModule,
